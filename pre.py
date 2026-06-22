86ce514078cfc5f6d660a351185fe6acc356dda0
Okttenda <johanna.wisniewski07@gmail.com>
2026-06-22 21:11:50 +0200

    added local Playlist saving

--- pre.py
+++ pre.py
@@ -1,10 +1,8 @@
-import texts
+from Spotify import spotifyAuth
 import json
-import random
-import time
-import sys
 import main
-
+import texts
+import time, sys, random, pathlib
 try:
     import spotipy, dotenv
 except:
@@ -12,18 +10,55 @@ except:
     time.sleep(3)
     sys.exit()
 
-from Spotify import spotifyAuth
-
+jsonFile = pathlib.Path("playlist_tracks.json")
 main.importantInformation()
 scope = "streaming user-read-playback-state playlist-read-collaborative"
 sp = spotifyAuth(scope=scope)
 playlist_ID = "5YdHUSd8VKnNZgdEzEgjpm"
-print("\n\n It may take some time to compile the whole Playlist if it is very big\n\n")
-tracks = main.extractTracks(playlistID=playlist_ID, sp=sp)
+print("Collecting Playlist Tracks...")
+
+print("[1]Load JSON file | [2]Download Playlist Tracks")
+result = int(input())
+if result == 1:
+    if jsonFile.exists():
+        with open(jsonFile) as f:
+            data = json.load(f)
+    else:
+        print("There is no loaded Plalist.")
+        print("Loading Playlist from Spotify... This can take a while...")
+        tracks = main.extractTracks(playlistID=playlist_ID, sp=sp)
+
+        t = {
+        "1":tracks
+        }
+
+        with open(jsonFile, "w") as f:
+            json.dump(t, f, indent=4)
+
+elif result == 2:
+    print("Loading Playlist from Spotify... This can take a while...")
+    tracks = main.extractTracks(playlistID=playlist_ID, sp=sp)
+
+    t = {
+    "1":tracks
+    }
+
+    with open(jsonFile, "w") as f:
+        json.dump(t, f, indent=4)
+
+    sys.exit()
+
+list = data["1"]
+
 while True:
-    print("Playing random Song from Playlist with Playlist ID:" + playlist_ID)
+    track = random.choice(list)
+    playing_tracks = f"https://open.spotify.com/track/{track}"
+    
+    print("Playing random Song from Playlist with Playlist ID:" + playing_tracks)
     print("\n\n")
-    main.playRandomTrack(trackList=tracks, sp=sp)
+    playing_track = [playing_tracks]
+    sp.start_playback(uris=playing_track)
+
     input("To stop the Music press Enter\n\n")
     sp.pause_playback()
     input("If you want to play the next Song press Enter\n\n")
